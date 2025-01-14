34ef77fb42edc3cc31f5064d0b3e3c3e45cbb2fb
Wins444 <winsking04yt@gmail.com>
2025-01-14 13:37:36 +0000

    Excuse Generator

--- src/app.js
+++ src/app.js
@@ -6,6 +6,27 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let who = ["Un dragón", "Mi profesor", "Un robot", "Un ladrón"];
+  let action = ["robó", "destruyó", "perdió", "quemó"];
+  let what = ["mi tarea", "mi laptop", "mi celular", "mis zapatos"];
+  let when = [
+    "antes de llegar a clase",
+    "mientras dormía",
+    "cuando iba al supermercado",
+    "durante mi desayuno"
+  ];
+
+  let whoIndex = Math.floor(Math.random() * who.length);
+  let actionIndex = Math.floor(Math.random() * action.length);
+  let whatIndex = Math.floor(Math.random() * what.length);
+  let whenIndex = Math.floor(Math.random() * when.length);
+
+  document.querySelector("#excusa").innerHTML =
+    who[whoIndex] +
+    " " +
+    action[actionIndex] +
+    " " +
+    what[whatIndex] +
+    " " +
+    when[whenIndex];
 };
